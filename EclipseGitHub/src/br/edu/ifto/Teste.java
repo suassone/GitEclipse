fb2bb1617a472476e209e6d20c4913c1b2192c55
IFTO <IFTO@DESKTOP-EP7DH23>
2021-05-13 15:21:09 -0300

    upload Projeto Inicial

---
+++ EclipseGitHub/src/br/edu/ifto/Teste.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.edu.ifto;
+
+/**
+ * @author Jocivan Suassone Alves
+ *
+ */
+public class Teste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
